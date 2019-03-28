27e32ba5760918f9a077f257d738e10e6fc2f3c0
StefanoBuccelli <stefano.buccelli@gmail.com>
2019-03-28 14:42:42 +0100

    re organizing

--- Matlab_scripts/byc_get_table_burst.m
+++ Matlab_scripts/byc_get_table_burst.m
@@ -70,4 +70,47 @@ result_table=array2table(curr_chan_results,'VariableNames',var_names);
 result_table(isnan(result_table.sample_peak),:)=[];
 signal_low_mat=double(signal_low);
 
-save([data_result_folder 'results'],'result_table','signal_low_mat','signal','time_s','frequency_limits','fs_mat')
+
+%% defining burst parameters (note that first and last cycles have NaN in some of the relevant paramters for burst detection)
+
+% 'param.amplitude_fraction_threshold',.3,
+% 'param.amplitude_consistency_threshold',.4,
+% 'param.period_consistency_threshold',.5, 
+% 'param.monotonicity_threshold',.8,
+% 'param.N_cycles_min',3
+
+param.amplitude_fraction_threshold=[0 0 0 0];
+param.amplitude_consistency_threshold=[0.996 0.996 0.996 0.996];
+param.period_consistency_threshold=[.5 .5 .5 .5];
+param.monotonicity_threshold=[.8 .8 .8 .8];
+param.N_cycles_min=[2 3 4 5];
+
+%% 
+n_combinations=size(param.amplitude_fraction_threshold,2);
+is_burst_before_n_cycle=zeros(height(result_table),n_combinations,'logical');
+is_burst_comb=zeros(height(result_table),n_combinations);
+
+for curr_combination=1:n_combinations
+    amp_frac=result_table.period_consistency>param.amplitude_fraction_threshold(curr_combination);
+    amp_cons=result_table.amp_consistency>param.amplitude_consistency_threshold(curr_combination);
+    period_cons=result_table.period_consistency>param.period_consistency_threshold(curr_combination);
+    mono=result_table.monotonicity>param.monotonicity_threshold(curr_combination);
+    is_burst_before_n_cycle(:,curr_combination)=[amp_frac & amp_cons & period_cons & mono];
+    
+    %% remove
+    holes_pos_in_is_burst=find(is_burst_before_n_cycle(:,curr_combination)==0);
+    seq_len=diff(holes_pos_in_is_burst)-1;
+    start_seq=holes_pos_in_is_burst(1:end-1)+1;
+    
+    pos_above_N_in_holes=find(seq_len>=param.N_cycles_min(curr_combination));
+    start_seq_above_N=start_seq(pos_above_N_in_holes);
+    seq_len_above_N=seq_len(pos_above_N_in_holes);
+
+    for curr_seq_above=1:length(start_seq_above_N)
+        start=start_seq_above_N(curr_seq_above);
+        is_burst_comb(start:start+seq_len_above_N(curr_seq_above)-1,curr_combination)=1;
+    end
+end
+
+%%
+save([data_result_folder 'results'],'result_table','signal_low_mat','signal','time_s','frequency_limits','fs_mat','param','is_burst_comb')
--- Matlab_scripts/byc_plot_bursts.m
+++ Matlab_scripts/byc_plot_bursts.m
@@ -6,28 +6,26 @@ data_result_folder='C:\Users\BuccelliLab\Documents\GitHub\bycycle_matlab\Results
 fig_folder='C:\Users\BuccelliLab\Documents\GitHub\bycycle_matlab\Results\Results_fig\';
 load([data_result_folder 'results'])
 
-%% showing burst-related parameters
-figure
-h_hist(1)=subplot(2,2,1);
-histogram(result_table.amp_consistency,'normalization','probability')
-title('amp consistency')
-h_hist(2)=subplot(2,2,2);
-histogram(result_table.amp_fraction,'normalization','probability')
-title('amp fraction')
-h_hist(3)=subplot(2,2,3);
-histogram(result_table.period_consistency,'normalization','probability')
-title('period consistency')
-h_hist(4)=subplot(2,2,4);
-histogram(result_table.monotonicity,'normalization','probability')
-title('monotonicity')
-savefig([fig_folder 'histogram_burst_params'])
-
 %% burst no burst cycle
+n_params=length(param.amplitude_consistency_threshold);
 figure
-histogram(result_table.is_burst,'normalization','probability')
-xticks([0 1])
-xticklabels({'FALSE','TRUE'})
-ylim([0 1])
-ylabel('probability')
-title('cycle is burst')
+for curr_comb=1:n_params
+    subplot(ceil(sqrt(n_params)),round(sqrt(n_params)),curr_comb)
+    histogram(is_burst_comb(:,curr_comb),'normalization','probability')
+    xticks([0 1])
+    xticklabels({'FALSE','TRUE'})
+    ylim([0 1])
+    ylabel('probability')
+    title(['curr comb: ' num2str(curr_comb)])
+end
 savefig([fig_folder 'histogram_burst_cycle_prob'])
+
+%%
+
+
+
+
+
+
+
+
